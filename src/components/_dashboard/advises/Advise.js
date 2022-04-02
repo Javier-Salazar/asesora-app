e888bc5e5a16a077909339d91805055e5b762ade
Javier Alexis Salazar Ballesteros <javiersalazar1945@outlook.com>
2022-04-01 20:36:18 -0600

    se modifa advises

---
+++ src/components/_dashboard/advises/Advise.js
@@ -0,0 +1,66 @@
+import { Link as RouterLink } from 'react-router-dom';
+import styled from '@emotion/styled';
+import { Icon } from '@iconify/react';
+import startFill from '@iconify/icons-eva/star-fill';
+import messageCircleFill from '@iconify/icons-eva/message-circle-fill';
+import { Typography, Box, Stack, Button, Grid, Tooltip, Chip, Avatar } from '@mui/material';
+import MockImgAvatar from '../../../utils/mockImages';
+
+const ChipStyled = styled(Chip)(({theme}) => ({
+    color: theme.palette.primary.main,
+    backgroundColor: '#EBF8F6'
+}));
+
+function Advise(props) {
+    return (
+        <Stack
+            alignItems="center"
+            spacing={0}
+            sx={{p: 2}}
+        >
+            <div style={{display: 'flex', justifyContent: 'space-between', alignItems: 'center', width: '100%'}}>
+                <Typography gutterBottom variant="h6" sx={{m: 0}}>
+                    Redisign Website
+                </Typography>
+            </div>
+
+            <Box sx={{ flex: 'flex' , textAlign: 'left', border: '1px blue solid', mt: 1 }}>
+                <div style={{display: 'flex', justifyContent: 'space-between', flexWrap: 'wrap'}}>
+                    <ChipStyled label="Matemáticas" sx={{mb: 1}} />
+                    <ChipStyled label="Programacion" sx={{mb: 1}} />
+                    <ChipStyled label="investigacion" />
+                    <ChipStyled label="Matemáticas" />
+                </div>
+
+                <div style={{display: 'flex', justifyContent: 'space-between', marginTop: '16px'}}>
+                    <div style={{display: 'inline-flex'}}>
+                        <Avatar src={`data:image/png;base64,${MockImgAvatar()}`} alt="avatar_1" 
+                            sx={{ width: 35, height: 35 }}
+                        />
+                        <Typography variant="body2" sx={{ color: 'text.secondary', display: 'flex', alignItems: 'center', pl: 1 }}>
+                            Margarita B.
+                        </Typography>
+                    </div>
+                    
+                    <div style={{display: 'inline-flex'}}>
+                        <Typography variant="body2" sx={{ color: 'text.secondary', display: 'flex', alignItems: 'center'}}>
+                            6&nbsp;
+                            <Icon icon={messageCircleFill} />
+                        </Typography>
+                        <Typography variant="body2" sx={{ color: 'text.secondary', display: 'flex', alignItems: 'center', pl: 1 }}>
+                            3&nbsp;
+                            <Icon icon={startFill} />
+                        </Typography>
+                    </div>
+                </div>
+            </Box>
+
+            <Grid container columnSpacing={0} sx={{mt: 3}}>
+                <Button fullWidth>
+                    agendar
+                </Button>
+            </Grid>
+        </Stack>
+    );
+}
+export default Advise;
--- src/pages/Advises.js
+++ src/pages/Advises.js
@@ -1,84 +1,25 @@
-import { useState } from 'react';
-import { Link as RouterLink } from 'react-router-dom';
-import styled from '@emotion/styled';
-import { Icon } from '@iconify/react';
-import startFill from '@iconify/icons-eva/star-fill';
-import messageCircleFill from '@iconify/icons-eva/message-circle-fill';
-import heartOutline from '@iconify/icons-eva/heart-outline';
-import heartFill from '@iconify/icons-eva/heart-fill';
-import { Typography, Box, Stack, Button, Grid, IconButton, Tooltip, Chip } from '@mui/material';
+import { Container, Typography, Grid, Card } from '@mui/material';
+import Page from '../components/Page';
+import { Advise } from '../components/_dashboard/advises';
 
-const ChipStyled = styled(Chip)(({theme}) => ({
-    color: theme.palette.primary.main,
-    backgroundColor: '#EBF8F6'
-}));
-
-function Advises(props) {
-    const [like, setLike] = useState(false);
-
-    const handleLike = () => {
-        setLike(!like);
-    }
-
+function Advises() {
     return (
-        <Stack
-            alignItems="center"
-            spacing={0}
-            sx={{p: 2}}
-        >
-            <div style={{display: 'flex', justifyContent: 'space-between', alignItems: 'center', width: '100%'}}>
-                <Typography gutterBottom variant="h6" sx={{m: 0}}>
-                    {props.name}
+        <Page title="AsesoraApp | Asesorías">
+            <Container>
+                <Typography variant="h4" sx={{ mb: 5 }}>
+                    Asesorías
                 </Typography>
-                <IconButton color="error" size="small" onClick={handleLike} style={{alignSelf: 'flex-start'}}>
-                    {
-                        like 
-                        ?
-                            <Tooltip title="Quitar de favoritos" placement="top" arrow>
-                                <Icon icon={heartFill} width="32px" />
-                            </Tooltip>
-                            
-                        : 
-                            <Tooltip title="Añadir a favoritos" placement="top" arrow>
-                                <Icon icon={heartOutline} width="32px" />
-                            </Tooltip>
-                    }
-                </IconButton>
-            </div>
 
-            <Box sx={{ flex: 'flex' , textAlign: 'left', border: '1px blue solid', mt: 1 }}>
-                <div style={{display: 'flex', justifyContent: 'space-between', flexWrap: 'wrap'}}>
-                    <ChipStyled label="Matemáticas" sx={{mb: 1}} />
-                    <ChipStyled label="Programacion" sx={{mb: 1}} />
-                    <ChipStyled label="investigacion" />
-                    <ChipStyled label="Matemáticas" />
-                </div>
-
-                <div style={{display: 'inline-flex'}}>
-                    <Typography variant="body2" sx={{ color: 'text.secondary', display: 'flex', alignItems: 'center'}}>
-                        {props.rating}&nbsp;
-                        <Icon icon={startFill} />
-                    </Typography>
-                    <Typography variant="body2" sx={{ color: 'text.secondary', display: 'flex', alignItems: 'center', pl: 2 }}>
-                        {props.comments}&nbsp;
-                        <Icon icon={messageCircleFill} />
-                    </Typography>
-                </div>
-            </Box>
-
-            <Grid container columnSpacing={0} sx={{mt: 3}}>
-                <Grid item xs={6} sm={6}>
-                    <Button fullWidth to="#" component={RouterLink}>
-                        ver perfil
-                    </Button>
+                <Grid container spacing={3}>
+                    <Grid item xs={12} sm={6} md={3}>
+                        <Card>
+                            <Advise />
+                        </Card>
+                    </Grid>
                 </Grid>
-                <Grid item xs={6} sm={6}>
-                    <Button fullWidth to="#" component={RouterLink}>
-                        agendar
-                    </Button>
-                </Grid>
-            </Grid>
-        </Stack>
+            </Container>
+        </Page>
     );
 }
+
 export default Advises;
