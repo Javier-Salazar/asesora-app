0136e88bf93bcbedc4490b8d3671c591ff95a4f1
Javier Alexis Salazar Ballesteros <63878119+Javier-Salazar@users.noreply.github.com>
2022-05-10 21:42:00 -0600

    Update SidebarConfigAdvisor.js

--- src/layouts/dashboard/SidebarConfigAdvisor.js
+++ src/layouts/dashboard/SidebarConfigAdvisor.js
@@ -1,16 +1,11 @@
 import { Icon } from '@iconify/react';
 import gridFill from '@iconify/icons-eva/grid-fill';
-import peopleFill from '@iconify/icons-eva/people-fill';
-import personDoneFill from '@iconify/icons-eva/person-done-fill';
 import fileTextFill from '@iconify/icons-eva/file-text-fill';
 import bookFill from '@iconify/icons-eva/book-fill';
 import clipBoardFill from '@iconify/icons-eva/clipboard-fill';
 import calendarFill from '@iconify/icons-eva/calendar-fill';
 import clockFill from '@iconify/icons-eva/clock-fill';
 import infoFill from '@iconify/icons-eva/info-fill';
-import fileAddFill from '@iconify/icons-eva/file-add-fill';
-import pinFill from '@iconify/icons-eva/pin-fill';
-import EditFill from '@iconify/icons-eva/edit-fill';
 
 const getIcon = (name) => <Icon icon={name} width={22} height={22} />;
 
@@ -24,11 +19,6 @@ const sidebarConfigAdvisor = [
     title: 'materias',
     path: '/dashboard/subject',
     icon: getIcon(fileTextFill)
-  },
-  {
-    title: 'añadir materia',
-    path: '/dashboard/new-subject',
-    icon: getIcon(fileAddFill)
   },
   {
     title: 'añadir asesoría',
